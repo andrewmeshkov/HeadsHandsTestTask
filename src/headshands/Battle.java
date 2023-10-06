a546b11ca56b20763b06ca71737df92b86a2d8e9
andrewmeshkov <50665929+andrewmeshkov@users.noreply.github.com>
2023-10-06 17:47:53 +0300

    Update Battle.java

--- src/headshands/Battle.java
+++ src/headshands/Battle.java
@@ -1,6 +1,7 @@
 package headshands;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 /*
  * Дополнительный класс Battle, которые позволяет провести бой между двумя командами
@@ -18,6 +19,8 @@ public class Battle {
 		this.started = false;
 		this.firstTeam = new ArrayList<>();
 		this.secondTeam = new ArrayList<>();
+		this.fillTeam(1, new Scanner(System.in));
+		this.fillTeam(2, new Scanner(System.in));
 	}
 	
 	void addToFirstTeam(Creature c) {
@@ -37,6 +40,47 @@ public class Battle {
 		}
 	}
 	
+	static boolean checkArgs(int attack, int defend, int health, int leftDamage, int rigthDamage) {
+		boolean result = true;
+		result &= (1 <= attack && attack <= 30);
+		result &= (1 <= defend && defend <= 30);
+		result &= (health > 0);
+		result &= (rigthDamage > leftDamage && leftDamage > 0);
+		return result;
+	}
+	
+	static int[] readStats(Scanner in) {
+		int attack = in.nextInt();
+		int defend = in.nextInt();
+		int health = in.nextInt();
+		int leftDamage = in.nextInt();
+		int rigthDamage = in.nextInt();
+		return new int[]{attack, defend, health, leftDamage, rigthDamage};
+	}
+	
+	 void fillTeam(int number, Scanner in) {
+
+		System.out.printf("Введите количество существ в %s команде : \n", number==1 ? "первой" : "второй");
+		int count = in.nextInt();
+		for(int i = 0; i < count;i++) {
+			System.out.println("Введите тип существа (игрок/монстр)");
+			String type = in.next();
+			int[] stats = readStats(in);
+			while(!checkArgs(stats[0], stats[1], stats[2], stats[3], stats[4])) {
+				System.out.println("Введите данные заново");
+				stats = readStats(in);
+			}
+			Creature creature = (type == "игрок") ? 
+					new Player(stats[0], stats[1], stats[2], stats[3], stats[4]) 
+					: new Monster(stats[0], stats[1], stats[2], stats[3], stats[4]);
+			if(number == 1) {
+				this.addToFirstTeam(creature);
+			}
+			else {
+				this.addToSecondTeam(creature);
+			}
+		}
+	}
 	
 	void startBattle() {
 		this.started = true;
